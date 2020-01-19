197303098417a059ca0a6f0538deecf5a81ed880
SeverettH <22439127+SeverettH@users.noreply.github.com>
2020-01-19 17:01:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Everett Houston
+"Weekend Projects"
+Meditation app
+Jan 20th, 2020
+
+
+This application allows a user to select from 3 predefined time durations (2/5/10) minutes.
+
+Which will then be displayed below the 'Play'/'Pause' button.
+
+Upon clicking on the play button the selected timer will begin to count down, with this
+a progress bar will fill in around the play button at the respective rate needed for each duration of time.
+
+In the background a user will either be presented with a video of rain or waves at the beach.
+Each of the videos is accompanied by their respective sounds.
+A user is able to toggle between the two video background by selecting either of the two buttons
+on the right side of the page, which display rain clouds or a sun.
+
+A user is able to pause the counter, at which time the current time will stop as well as the
+audio track, however the background video will continue to loop.
+
+
+
+Objective:
+
+          During this portion of "weekend projects", I am re-familiarizing myself with
+          HTML/CSS/Vanilla Js syntax. And slowly aiding in additional libraries, and functionalities
+          with each project.
+
+          As someone who enjoys meditation/ having a break from programming throughout the day I wanted
+          to create a simple app which provides users with timer accompanied by ambient noise and
+          relaxing visuals that they could relax to, which was the inspiration for this quick project.
---
+++ app.js
@@ -0,0 +1,77 @@
+const app = () => {
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.vid-container video');
+
+  //sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+  //time display
+  const timeDisplay = document.querySelector('.time-display');
+  const timeSelect = document.querySelectorAll('.time-select button');
+  //length of outline
+  const outlineLength = outline.getTotalLength();
+  //duration
+  let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+  //select different sounds
+  sounds.forEach(sound =>{
+    sound.addEventListener('click', function(){
+      song.src = this.getAttribute('data-sound');
+      video.src = this.getAttribute('data-video');
+      checkPlaying(song);
+    });
+  });
+
+  //play sound
+  play.addEventListener("click", () => {
+      checkPlaying(song);
+  });
+
+//select sound
+  timeSelect.forEach(option =>{
+    option.addEventListener('click', function(){
+      fakeDuration = this.getAttribute('data-time');
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+    });
+  });
+
+  //function to stop & play sounds
+  const checkPlaying = song =>{
+    if(song.paused){
+        song.play();
+        video.play();
+        play.src = './svg/pause.svg';
+    }else{
+      song.pause();
+      video.play();
+      play.src = './svg/play.svg';
+    }
+  }
+  //circle animation
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    //Text animation
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if(currentTime >= fakeDuration){
+      song.pause();
+      song.currentTime = 0;
+      play.src = './svg.play.svg';
+      video.pause();
+    }
+  };
+};
+
+
+
+app();
